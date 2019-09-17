1e69acc4addbac4dfc5a7a15e4a470bee518b35f
Rakshit Sinha <rakshit.sinha@oracle.com>
2019-09-17 07:13:59 -0700

    Finished all the code for the Demo

--- src/hooks/UseClassesExample.js
+++ src/hooks/UseClassesExample.js
@@ -1,11 +1,91 @@
 import React from 'react';
+import { ThemeContext, LocaleContext } from '../context';
 
-const UseClassesExample = () => {
-  return (
-    <React.Fragment>
-      <section className="row">From class component</section>
-    </React.Fragment>
-  );
-};
+class UseClassesExample extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Rakshit',
+      surname: 'Sinha',
+      width: window.innerWidth,
+    };
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSurnameChange = this.handleSurnameChange.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+    const { name, surname } = this.state;
+    document.title = `${name} ${surname}`;
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentDidUpdate() {
+    const { name, surname } = this.state;
+    document.title = `${name} ${surname}`;
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
+  handleResize() {
+    this.setState({
+      width: window.innerWidth,
+    });
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handleSurnameChange(e) {
+    this.setState({
+      surname: e.target.value,
+    });
+  }
+
+  render() {
+    const { name, surname, width } = this.state;
+    return (
+      <ThemeContext.Consumer>
+        {({ theme }) => (
+          <React.Fragment>
+            <section className={`row ${theme.dark}`}>
+              <span>Name</span>
+              <input
+                type="text"
+                value={name}
+                onChange={this.handleNameChange}
+              />
+            </section>
+            <section className={`row ${theme.light}`}>
+              <span>Surname</span>
+              <input
+                type="text"
+                value={surname}
+                onChange={this.handleSurnameChange}
+              />
+            </section>
+            <LocaleContext.Consumer>
+              {({ locale }) => (
+                <section className={`row ${theme.dark}`}>
+                  <span>Language</span>
+                  <input type="text" value={locale.local} />
+                </section>
+              )}
+            </LocaleContext.Consumer>
+            <section className={`row ${theme.light}`}>
+              <span>Width</span>
+              <input value={width} />
+            </section>
+          </React.Fragment>
+        )}
+      </ThemeContext.Consumer>
+    );
+  }
+}
 
 export default UseClassesExample;
--- src/hooks/UseHooksExample.js
+++ src/hooks/UseHooksExample.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { ThemeContext, LocaleContext } from '../context';
+
+const useWindowWidth = () => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  React.useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  });
+  return width;
+};
+
+const useDocumentTitle = title => {
+  React.useEffect(() => {
+    document.title = title;
+  });
+};
+
+const useFormInput = initialValue => {
+  const [value, setValue] = React.useState(initialValue);
+
+  const handleNameChange = e => {
+    setValue(e.target.value);
+  };
+
+  return {
+    value,
+    onChange: handleNameChange,
+  };
+};
 
 const useHooksExample = () => {
+  const name = useFormInput('Rakshit');
+  const surname = useFormInput('Sinha');
+  const { theme } = React.useContext(ThemeContext);
+  const { locale } = React.useContext(LocaleContext);
+  const width = useWindowWidth();
+  useDocumentTitle(`${name.value} ${surname.value}`);
+
   return (
     <React.Fragment>
-      <section className="row">From function component</section>
+      <section className={`row ${theme.dark}`}>
+        <span>Name</span>
+        <input type="text" {...name} />
+      </section>
+      <section className={`row ${theme.light}`}>
+        <span>Surname</span>
+        <input type="text" {...surname} />
+      </section>
+      <section className={`row ${theme.dark}`}>
+        <span>Language</span>
+        <input type="text" value={locale.foreign} />
+      </section>
+      <section className={`row ${theme.light}`}>
+        <span>Width</span>
+        <input type="text" value={width} />
+      </section>
     </React.Fragment>
   );
 };
