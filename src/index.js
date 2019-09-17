5a58a35c5e52d5128ecb273273b6977b0ed4eff1
Rakshit Sinha <rakshit.sinha@oracle.com>
2019-09-17 03:08:48 -0700

    Initial commit
    * Changed styles across the entire app
    * Created a new directory structure for blank/boilerplate examples

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Styles
@@ -24,97 +24,102 @@ const VISIBLE = {
   IS_REACT_SPRING_2_VISIBLE: 'Fade',
 };
 
-class App extends Component {
-  state = {
+const App = () => {
+  const state = {
     isVisible: [
-      VISIBLE.IS_REACT_SPRING_1_VISIBLE,
-      VISIBLE.IS_REACT_SPRING_2_VISIBLE,
+      VISIBLE.IS_USE_STATE_VISIBLE,
+      VISIBLE.IS_USE_CONTEXT_VISIBLE,
+      VISIBLE.IS_USE_EFFECT_VISIBLE,
+      VISIBLE.IS_USE_CUSTOM_HOOKS_VISIBLE,
+      VISIBLE.IS_USE_REDUCER_VISIBLE,
     ],
   };
 
-  render() {
-    const { isVisible } = this.state;
-    return (
-      <div>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_USE_STATE_VISIBLE)}
-          heading="useState"
-          sectionBg="purple-bg"
-        >
-          <div>
-            <h1 className="heading">new way</h1>
-            <Greeting />
-          </div>
-          <div>
-            <h1 className="heading">old way</h1>
-            <GreetingClass />
-          </div>
-        </Section>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_USE_CONTEXT_VISIBLE)}
-          heading="useContext"
-          sectionBg="white-bg"
-        >
-          <div>
-            <h1 className="heading">new way</h1>
-            <Context />
-          </div>
-          <div>
-            <h1 className="heading">old way</h1>
-            <ContextClass />
-          </div>
-        </Section>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_USE_EFFECT_VISIBLE)}
-          heading="useEffect"
-          sectionBg="gainsboro-bg"
-        >
-          <div>
-            <h1 className="heading">new way</h1>
-            <Title />
-          </div>
-          <div>
-            <h1 className="heading">old way</h1>
-            <TitleClass />
-          </div>
-        </Section>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_USE_CUSTOM_HOOKS_VISIBLE)}
-          heading="useCustomHooks"
-          sectionBg="purple-bg"
-        >
-          <div>
-            <h1 className="heading">new way</h1>
-            <CustomHook />
-          </div>
-        </Section>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_USE_REDUCER_VISIBLE)}
-          heading="useReducer"
-          sectionBg="white-bg"
-        >
-          <div>
-            <h1 className="heading">Count Example</h1>
-            <PokemonInfo />
-          </div>
-        </Section>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_REACT_SPRING_1_VISIBLE)}
-          heading="React Spring Progress Bar Example"
-          sectionBg="gainsboro-bg"
-        >
-          <Progress />
-        </Section>
-        <Section
-          isVisible={isVisible.includes(VISIBLE.IS_REACT_SPRING_2_VISIBLE)}
-          heading="React Spring Fade Bar Example"
-          sectionBg="gainsboro-bg"
-        >
-          <Fade />
-        </Section>
-      </div>
-    );
-  }
-}
+  const { isVisible } = state;
+  const HOOKS_HEADING = <h1 className="heading">Hooks Way</h1>;
+  const CLASSES_HEADING = <h1 className="heading">Classes way</h1>;
+
+  return (
+    <div>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_USE_STATE_VISIBLE)}
+        heading="useState"
+        sectionBg="example-gradient-bg"
+      >
+        <div>
+          {HOOKS_HEADING}
+          <Greeting />
+        </div>
+        <div>
+          {CLASSES_HEADING}
+          <GreetingClass />
+        </div>
+      </Section>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_USE_CONTEXT_VISIBLE)}
+        heading="useContext"
+        sectionBg="example-gradient-bg"
+      >
+        <div>
+          {HOOKS_HEADING}
+          <Context />
+        </div>
+        <div>
+          {CLASSES_HEADING}
+          <ContextClass />
+        </div>
+      </Section>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_USE_EFFECT_VISIBLE)}
+        heading="useEffect"
+        sectionBg="example-gradient-bg"
+      >
+        <div>
+          {HOOKS_HEADING}
+          <Title />
+        </div>
+        <div>
+          {CLASSES_HEADING}
+          <h1 className="heading">old way</h1>
+          <TitleClass />
+        </div>
+      </Section>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_USE_CUSTOM_HOOKS_VISIBLE)}
+        heading="useCustomHooks"
+        sectionBg="example-gradient-bg"
+      >
+        <div>
+          {HOOKS_HEADING}
+          <CustomHook />
+        </div>
+      </Section>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_USE_REDUCER_VISIBLE)}
+        heading="useReducer"
+        sectionBg="example-gradient-bg"
+      >
+        <div>
+          <h1 className="heading">Count Example</h1>
+          <PokemonInfo />
+        </div>
+      </Section>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_REACT_SPRING_1_VISIBLE)}
+        heading="React Spring Progress Bar Example"
+        sectionBg="gainsboro-bg"
+      >
+        <Progress />
+      </Section>
+      <Section
+        isVisible={isVisible.includes(VISIBLE.IS_REACT_SPRING_2_VISIBLE)}
+        heading="React Spring Fade Bar Example"
+        sectionBg="gainsboro-bg"
+      >
+        <Fade />
+      </Section>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
